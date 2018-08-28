d1d2726888ae37937182692f2ba999ad28fffbef
Anderson Mendes <37872453+andersonmendesdev@users.noreply.github.com>
2018-08-28 16:08:09 -0400

    correction
    correction

--- controllers/sendingImage.js
+++ controllers/sendingImage.js
@@ -1,16 +1,16 @@
-    const aws = require('aws-sdk')
-    const multer = require('multer')
-    const multerS3 = require('multer-s3')
-    const { accessKeyId, secretAccessKey, region, Bucket } = require('../config/aws-config.json')
+const aws = require('aws-sdk')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const { accessKeyId, secretAccessKey, region, Bucket } = require('../config/aws-config.json')
 
-    aws.config = new aws.Config({
-        accessKeyId: process.env.S3_ACCESS_KEY || accessKeyId,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || secretAccessKey,
-        region: process.env.S3_REGION || region
-    })
-    const s3 = new aws.S3()
-    const upload = multer({
-        storage: multerS3({
+aws.config = new aws.Config({
+    accessKeyId: process.env.S3_ACCESS_KEY || accessKeyId,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || secretAccessKey,
+    region: process.env.S3_REGION || region
+})
+const s3 = new aws.S3()
+const upload = multer({
+    storage: multerS3({
         s3: s3,
         acl: 'public-read',
         bucket: Bucket,
@@ -20,7 +20,7 @@
         key: function (req, file, cb) {
             cb(null, file.originalname)
         }
-        })
     })
+})
 
-    module.exports = upload
+module.exports = upload
--- controllers/uploader.js
+++ controllers/uploader.js
@@ -4,23 +4,23 @@ import multerS3 from 'multer-s3'
 import {accessKeyId, secretAccessKey, region, Bucket} from '../config/aws-config.json'
 
 aws.config = new aws.Config({
-    accessKeyId: process.env.S3_ACCESS_KEY || accessKeyId,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || secretAccessKey,
-    region: process.env.S3_REGION || region
+  accessKeyId: process.env.S3_ACCESS_KEY || accessKeyId,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || secretAccessKey,
+  region: process.env.S3_REGION || region
 })
-const s3SDK = new aws.S3()
+const s3 = new aws.S3()
 const upload = multer({
-    storage: multerS3({
-      s3: s3SDK,
+  storage: multerS3({
+      s3: s3,
       acl: 'public-read',
       bucket: Bucket,
       contentType: function(req, file,cb){
-        cb(null, file.mimetype)
+          cb(null, file.mimetype)
       },
       key: function (req, file, cb) {
-        cb(null, file.originalname)
+          cb(null, file.originalname)
       }
-    })
+  })
 })
 
 export default upload
